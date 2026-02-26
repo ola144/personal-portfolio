af6159530fd9622828772ab1b2556fc379687f72
Ola <oladimejiagbaje@gmail.com>
2026-02-26 17:41:01 +0100

    added new project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,66 +1,121 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Title from "./Title";
 import { projectList, type IProject } from "../model/projects";
-
-// import busBooking from "/images/busBooking-app.jpg";
-// import taskApp from "/images/task-app.jpg";
-// import onlineShop from "/images/online-shop.jpg";
+import { tns, type TinySliderInstance } from "tiny-slider";
 
 const Project = () => {
   const descprition =
     "Whether you have a design idea that needs to come to life or a website that requires a facelift, I'm here to turn your digital dreams into reality.";
 
   const [projects, setProjects] = useState<IProject[]>([]);
+  const sliderRef = useRef(null);
+  const sliderInstance = useRef<TinySliderInstance | null>(null);
 
   useEffect(() => {
     setProjects(projectList);
+
+    if (!sliderRef.current) return;
+
+    sliderInstance.current = tns({
+      container: sliderRef.current,
+      items: 3,
+      gutter: 10,
+      slideBy: 1,
+      autoplay: false,
+      autoplayTimeout: 3000,
+      autoplayButtonOutput: false,
+
+      controls: true, // arrows
+      controlsText: ["←", "→"],
+
+      nav: true, // 👈 pagination dots
+      navPosition: "bottom",
+
+      mouseDrag: true,
+      touch: true,
+
+      loop: true,
+
+      responsive: {
+        0: { items: 1 },
+        640: { items: 2 },
+        1024: { items: 3 },
+      },
+    });
+
+    return () => {
+      if (sliderInstance.current) {
+        sliderInstance.current.destroy();
+      }
+    };
   }, []);
+
+  // const initializeSlider = () => {
+  //   if (!sliderRef.current) return;
+
+  // };
+
+  //  pauseSlider() {
+  //   this.slideInstance?.pause();
+  // }
+
+  // playSlider() {
+  //   this.slideInstance?.play();
+  // }
 
   return (
     <section className="parent parent2">
       <Title title1="" title2="my projects" decscription={descprition} />
-      <div className="">
-        <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1  gap-x-8 gap-y-10 w-full">
-          {projects.map((project, index) => (
-            <div key={index} className="w-full h-fit">
-              {project.type === "shop" && (
-                <img
-                  src="/images/online-shop.JPG"
-                  alt={project.title}
-                  className="w-full lg:h-[40%] md:h-[200px] rounded-xl mb-5"
-                />
-              )}
-              {project.type === "task" && (
-                <img
-                  src="/images/task-app.JPG"
-                  alt={project.title}
-                  className="w-full h-[40%] md:h-[200px] rounded-xl mb-5"
-                />
-              )}
-              {project.type === "mirror" && (
-                <img
-                  src="/images/mirror-project.JPG"
-                  alt={project.title}
-                  className="w-full h-[40%] md:h-[200px] rounded-xl mb-5"
-                />
-              )}
 
-              <h1 className="text-gray-800 font-bold text-xl mb-5 capitalize">
-                {project.title}
-              </h1>
-              <p className="text-sm leading-normal text-gray-400 font-normal text-justify mb-5">
-                {project.description}
-              </p>
-              <a
-                href={project.liveUrl}
-                target="_blank"
-                className="bg-black w-fit py-2 px-3 text-white uppercase text-sm rounded-lg"
-              >
-                take a look at live demo
-              </a>
-            </div>
-          ))}
-        </div>
+      <div className="flex gap-1 relative w-full" ref={sliderRef}>
+        {/* <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1  gap-x-8 gap-y-10 w-full"> */}
+        {projects.map((project, index) => (
+          <div key={index} className="w-full h-fit">
+            {project.type === "shop" && (
+              <img
+                src="/images/online-shop.JPG"
+                alt={project.title}
+                className="w-full lg:h-[40%] md:h-[200px] rounded-xl mb-5"
+              />
+            )}
+            {project.type === "exlusive" && (
+              <img
+                src="/images/exclusive-landing.JPG"
+                alt={project.title}
+                className="w-full lg:h-[40%] md:h-[200px] rounded-xl mb-5"
+              />
+            )}
+            {project.type === "task" && (
+              <img
+                src="/images/task-app.JPG"
+                alt={project.title}
+                className="w-full h-[40%] md:h-[200px] rounded-xl mb-5"
+              />
+            )}
+            {project.type === "mirror" && (
+              <img
+                src="/images/mirror-project.JPG"
+                alt={project.title}
+                className="w-full h-[40%] md:h-[200px] rounded-xl mb-5"
+              />
+            )}
+
+            <h1 className="text-gray-800 font-bold md:text-lg text-sm mb-5 capitalize">
+              {project.title}
+            </h1>
+            <p className="md:text-sm text-xs leading-normal text-gray-400 font-normal text-justify mb-5">
+              {project.description}
+            </p>
+            <a
+              href={project.liveUrl}
+              target="_blank"
+              className="bg-black w-fit py-2 px-3 text-white uppercase text-sm rounded-lg"
+            >
+              take a look at live demo
+            </a>
+          </div>
+        ))}
+        {/* </div> */}
       </div>
     </section>
   );
--- src/model/projects.ts
+++ src/model/projects.ts
@@ -7,11 +7,11 @@ export interface IProject {
 
 export const projectList: IProject[] = [
   {
-    title: "Online Shop Web App (React + TailwindCSS)",
+    title: "Exclusive Ecommerce Application (Angular + TailwindCSS + Appwrite)",
     description:
-      "A fully responsive e-commerce web application built with ReactJS and styled using TailwindCSS. The app features product listings, category filtering, a dynamic shopping cart, and a smooth checkout flow. State management is handled using React hooks/Redux, and the UI is optimized for both desktop and mobile devices. Designed with clean, reusable components and modern UI/UX principles.",
-    type: "shop",
-    liveUrl: "https://online-shop-ten-hazel.vercel.app/",
+      "A full-featured ecommerce application built with Angular, TailwindCSS, and Appwrite for backend services. The platform includes product listing, authentication, cart management, order processing, admin dashboard, and real-time database integration. Appwrite powers authentication, database, and storage, while Angular signals handle reactive state management. Designed with performance, scalability, and clean UI/UX in mind. ",
+    type: "exlusive",
+    liveUrl: "https://exclusive-swart-alpha.vercel.app/",
   },
   {
     title: "Task Management App (Angular + TailwindCSS)",
@@ -19,6 +19,13 @@ export const projectList: IProject[] = [
       "A responsive task management application built with Angular and styled using TailwindCSS. Admin can create, edit, and organize and assigned tasks with status tracking (To-Do, In Progress, Completed). The app leverages Angular’s component-based architecture and services for clean state management, with firebase backend persistence to retain tasks across sessions. Designed with a modern, intuitive UI to enhance productivity.",
     type: "task",
     liveUrl: "https://task-app-three-pi.vercel.app/",
+  },
+  {
+    title: "Online Shop Web App (React + TailwindCSS)",
+    description:
+      "A fully responsive e-commerce web application built with ReactJS and styled using TailwindCSS. The app features product listings, category filtering, a dynamic shopping cart, and a smooth checkout flow. State management is handled using React hooks/Redux, and the UI is optimized for both desktop and mobile devices. Designed with clean, reusable components and modern UI/UX principles.",
+    type: "shop",
+    liveUrl: "https://online-shop-ten-hazel.vercel.app/",
   },
   {
     title: "The Mirror (Mentorship Platform)",
